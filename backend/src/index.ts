581dd24258fc19fc2ee62f7fe335239d70fd5668
sebas1401 <salonzor2@miumg.edu.gt>
2026-05-07 01:50:28 -0600

    evita caida por errores async backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import 'express-async-errors'
 import cors from 'cors'
 import { authRouter }         from './routes/auth.routes'
 import { estudiantesRouter }  from './routes/estudiantes.routes'
